f5800b21d10ba39ce86f121c66f9daf06dde686a
JamesSc0tt <jamesscott.js95@gmail.com>
2019-06-09 01:23:54 +0100

    [MDT] 999.php require login

--- includes/header.php
+++ includes/header.php
@@ -17,7 +17,7 @@
 	
 	
 	
-	$require_log = Array('booking', 'dmv', 'warrants', 'bolos', 'admin');
+	$require_log = Array('booking', 'dmv', 'warrants', 'bolos', 'admin', '999');
 	if (in_array($curpage, $require_log, false)) {
 		if (!$logged) {
 			header("Location:https://gov.fsn.life/index.php?requirelogin");
@@ -37,19 +37,27 @@
 ?>
 <html>
     <head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link rel="stylesheet" href="/Fusion/Fusion-MDT/css/style.css">
-        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">
-        <link rel="stylesheet" href="css/style.css">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+		<!-- james stuffs -->
+			<script src="includes/global.js"></script>
+		
+		<!-- lewlew stuff -->
+			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+			<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+			<link rel="stylesheet" href="/Fusion/Fusion-MDT/css/style.css">
+			<link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
+			<link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">
+			<link rel="stylesheet" href="css/style.css">
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 		
 		<!-- james editor stuffs -->
 			<script
 			  src="https://code.jquery.com/jquery-3.4.1.min.js"
 			  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
 			  crossorigin="anonymous"></script>
+			  
+		<!-- james alert stuffs -->
+			<link rel="stylesheet" href="includes/notifications/notifications.css">
+			<script src="includes/notifications/notifications.js"></script>
     </head>
     <body>
         <nav class="nav-extended">
@@ -74,7 +82,6 @@
               <ul class="tabs tabs-transparent">
 				<?php if (!$logged) { ?>
 					<li class="tab"><a <?php if ($curpage == 'index') { echo 'class="active"'; }?> href="index.php">Home</a></li>
-					<li class="tab"><a <?php if ($curpage == '999') { echo 'class="active"'; }?> href="999.php">Incident Report</a></li>
 					<li class="tab"><a <?php if ($curpage == 'criminals') { echo 'class="active"'; }?> href="criminals.php">Criminal Database</a></li>
 					<li class="tab"><a <?php if ($curpage == 'penal') { echo 'class="active"'; }?> href="penal.php">Penal Code</a></li>
 				<?php } else { ?>
