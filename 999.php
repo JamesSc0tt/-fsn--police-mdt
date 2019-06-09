53f60f31abbbaf7f8f5d981e7e2463647cfce812
JamesSc0tt <jamesscott.js95@gmail.com>
2019-06-09 01:26:00 +0100

    [MDT] Reports page, link with warrants, cases

--- 999.php
+++ 999.php
@@ -1,38 +1,104 @@
 <?php
-
+$curpage = '999';
 include "includes/header.php"; // local
-
 ?>
 <div class="container">
-	<?php if (!$logged) { 
-			if (isset($_POST['title']) && isset($_POST['report'])) {
-				
-				die("<div class='card-panel green lighten-4' style='color:black'>Your report has been logged for review.</div>");
+	<?php
+		if (isset($_GET['new'])) {
+			if (isset($_GET['link'])) {
+				?>
+					<input id="linkWith" type="hidden" value="<?php echo $_GET['link'] ?>">
+					<input id="linkID" type="hidden" value="<?php echo $_GET['lid'] ?>">
+				<?php
+			} else {
+				?>
+					<input id="linkWith" type="hidden" value="false">
+					<input id="linkID" type="hidden" value="-1">
+				<?php
 			}
-	?>
-		<h5 class="d_h5">REPORT A CRIME</h5>
+	?>	
+		<h5 class="d_h5">Create report</h5>
 		<hr class="hr_title">
-		<p>Please note that this is not for emergency assistance, reports are not monitored 24/7 - call 911 if you need an officer or emergency services right now.</p>
-		<div class="row">
-			
-			<form class="col s12" style="width:100%" action="999.php?999" method="POST">
-				<div class="col input-field" style="width:100%;">
-					<input name="title" id="pass" type="text" class="input-field validate">
-					<label for="text" class="">Title</label>
+		<p>When the report has been created you will receive a reference # for you to lookup the report at a later date, this is the number you give to a
+		civilian for them to discuss the report with an officer</p>
+		<hr class="hr_title">
+		<div class="row">	
+			<div class="row">
+				<div class="input-field col s6" style="width:100%">
+					<i class="material-icons prefix">mode_edit</i>
+					<input name="report" id="reportTitle" class="materialize-textarea" style="width:94%" data-length="300" placeholder="REPORT TITLE"></input>
 				</div>
-				<div class="row">
-					<div class="input-field col s6" style="width:100%">
-						<i class="material-icons prefix">mode_edit</i>
-						<textarea name="report" id="icon_prefix2" class="materialize-textarea" rows="10" style="width:94%" data-length="300"></textarea>
-						<label for="icon_prefix2">Write out your report... Make sure to include contact details.</label>
-						<button class="btn waves-effect waves-light" type="submit" style="width:100%;">Submit</button>
-					</div>
+			</div>
+			<div class="row">
+				<!-- Include stylesheet -->
+				<link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+				<style>
+					.ql-toolbar {
+						background-color:rgba(255,255,255,0.6);
+					}
+					#editor {
+						background-color:rgba(255,255,255,0.1);
+					}
+				</style>
+				<!-- Create the editor container -->
+				<div id="editor">
+				  
 				</div>
-			</form>
+				<!-- Include the Quill library -->
+				<script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
+
+				<!-- Initialize Quill editor -->
+				<script>
+				  var quill = new Quill('#editor', {
+					theme: 'snow'
+				  });
+				</script>
+			</div>
+			<div class="row">
+				<a class="btn waves-effect waves-light" onclick="submitReport()" style="width:100%;">CREATE REPORT</a>
+				<script>
+					function submitReport() {
+						var linkWith = $('#linkWith').val()
+						var linkID = $('#linkID').val()
+						var title = $('#reportTitle').val()
+						if (title == '') {
+							window.createNotification({
+								closeOnClick: true,
+								displayCloseButton: true,
+								theme: 'error'
+							})({
+								title: 'System Error',
+								message: 'A title is required for a report.'
+							});
+							return;
+						}
+						var content = document.querySelector(".ql-editor")
+						if (content == '' || content == '<p><br></p>') {
+							window.createNotification({
+								closeOnClick: true,
+								displayCloseButton: true,
+								theme: 'error'
+							})({
+								title: 'System Error',
+								message: 'Your report cannot be empty'
+							});
+							return;
+						}
+						content = content.innerHTML.replace(/</g, "#REPL101").replace(/>/g, "#REPL102")
+						//console.log(content);
+						post('js_system/addReport.php', {
+							linkWith: linkWith,
+							linkID: linkID,
+							title: title,
+							content: content
+						});
+						//window.location.href = "999.php";
+					}
+				</script>
+			</div>
 		</div>
 	<?php 
-		} else {
-			if (isset($_GET['viewid'])) {
+		} elseif (isset($_GET['viewid'])) {
 	?>	
 				<h5 class="d_h5">Report title</h5>
 				<hr class="hr_title">
@@ -66,8 +132,14 @@ include "includes/header.php"; // local
 				  });
 				</script>
 	<?php
-			} else {
-	?>		
+		} else {
+			if (isset($_GET['submitted'])) {
+			?>
+				<div class='card-panel green lighten-4' style='color:black'><b>Request Completed</b><br>The report has been submitted.</div>
+			<?php
+			}
+	?>	
+		<a class="btn waves-effect waves-light" href="?new" style="width:100%;">NEW REPORT</a>
 		<h5 class="d_h5">OPEN REPORTS</h5>
 		<hr class="hr_title">
 		<p>These reports are awaiting resolving, if you resolve one please contact High Command/Command/DoJ to change it's status.</p>
@@ -103,7 +175,7 @@ include "includes/header.php"; // local
 				</tr>
 			</tbody>
 		</table>
-	<?php }} ?>
+	<?php } ?>
 </div>
 <script>
 	$(document).ready(function() {
